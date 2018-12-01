12f7234ff1ec5f87c22317f7889ed6f11dcb8c42
Shannon Duffy <shannonduffy954@gmail.com>
2018-12-01 14:10:09 -0500

    changed directory name in process_dataset.py

--- process_dataset.py
+++ process_dataset.py
@@ -10,7 +10,7 @@ from opencv_functions import *
 from utility_functions import *
 
 
-dirJaffe = 'datasets/jaffe'
+dirJaffe = 'jaffe'
 cropFlag = True # False disables image cropping
 plot_confusion = True
 
