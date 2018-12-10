8d5737a5c9b9c41854c2231f0c85a6afb9d65e28
Ubuntu <ubuntu@ip-172-31-27-4.ec2.internal>
2018-12-10 15:46:03 +0000

    weights and graph of resnet with adam

--- face_classification/src/train_emotion_classifier.py
+++ face_classification/src/train_emotion_classifier.py
@@ -11,7 +11,7 @@ from keras.callbacks import CSVLogger, ModelCheckpoint, EarlyStopping
 from keras.callbacks import ReduceLROnPlateau
 from keras.preprocessing.image import ImageDataGenerator
 
-from models.cnn import simple_CNN
+from models.resnet import ResNet18
 from utils.datasets import DataManager
 from utils.datasets import split_data
 from utils.preprocessor import preprocess_input
@@ -37,8 +37,8 @@ data_generator = ImageDataGenerator(
                         horizontal_flip=True)
 
 # model parameters/compilation
-model = simple_CNN(input_shape, num_classes)
-model.compile(optimizer='adam', loss='categorical_crossentropy',
+model = ResNet18(input_shape, num_classes)
+model.compile(optimizer='adagrad', loss='categorical_crossentropy',
               metrics=['accuracy'])
 model.summary()
 
