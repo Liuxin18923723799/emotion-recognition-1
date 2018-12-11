82500a5532a4b7b6af27c9e1189c49e562d71f8d
Ubuntu <ubuntu@ip-172-31-27-4.ec2.internal>
2018-12-11 03:41:39 +0000

    used resnet with lr = 1.4e-3

--- face_classification/src/train_emotion_classifier.py
+++ face_classification/src/train_emotion_classifier.py
@@ -10,6 +10,7 @@ import matplotlib.pyplot as plt
 from keras.callbacks import CSVLogger, ModelCheckpoint, EarlyStopping
 from keras.callbacks import ReduceLROnPlateau
 from keras.preprocessing.image import ImageDataGenerator
+from keras.optimizers import Adam
 
 from models.resnet import ResNet18
 from utils.datasets import DataManager
@@ -38,7 +39,8 @@ data_generator = ImageDataGenerator(
 
 # model parameters/compilation
 model = ResNet18(input_shape, num_classes)
-model.compile(optimizer='adagrad', loss='categorical_crossentropy',
+adam = Adam(lr=1.46e-3)
+model.compile(optimizer=adam, loss='categorical_crossentropy',
               metrics=['accuracy'])
 model.summary()
 
